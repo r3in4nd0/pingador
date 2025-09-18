76e32d2901df81d2ff47c59a63d42e83b718b864
r3in4nd0 <lrms2@discente.ifpe.edu.br>
2025-09-17 23:56:10 -0300

    settings, urls, views, templates initial

--- core/views.py
+++ core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import os  
+
+def pingar(request,ip):
+    command = f'ping -c 1 {ip}'
+    os.system(command)
+    return render(request,'core/index.html',{'ip':ip})
+def home(request):
+    return render(request,'core/index.html')
